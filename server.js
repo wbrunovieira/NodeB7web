6334f2b2783254c6af3c6f003ed3bf84f0bbd63c
wbrunovieira <wbrunovieira77@gmail.com>
2020-12-27 09:33:15 +0000

    added mongo connection

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 
-app.set('port', 7777);
+require('dotenv').config({path:'variables.env'});
+
+
+// connect to database
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true , useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (error)=>{
+    console.error("ERRO: "+error.message);
+})
+
+app.set('port', process.env.PORT || 7777);
 
 const server = app.listen(app.get('port'), ()=> {
     console.log("Servidor rodando na porta: "+server.address().port);
