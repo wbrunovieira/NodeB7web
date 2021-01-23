8d5acf71f3680ae57cca2f21be8be99ead626b0d
wbrunovieira <wbrunovieira77@gmail.com>
2021-01-23 16:29:35 +0000

    connection com MYSQL

---
+++ devnotesApi/src/controllers/NoteController.js
@@ -0,0 +1,23 @@
+const NoteServices = require('../services/NoteServices');
+
+
+module.exports = {
+    ping: (req, res) => {
+        res.json({pong:true});
+    },
+    all: () => {
+
+    },
+    one: () => {
+
+    },
+    new: () => {
+
+    },
+    edit: () => {
+
+    },
+    delete: () => {
+
+    },
+};
