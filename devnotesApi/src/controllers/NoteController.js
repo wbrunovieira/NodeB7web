7c8fc693db06dfbdb72461d24ce73e215a7a3db6
wbrunovieira <wbrunovieira77@gmail.com>
2021-02-01 10:25:35 +0000

    finished the crud

--- devnotesApi/src/controllers/NoteController.js
+++ devnotesApi/src/controllers/NoteController.js
@@ -1,23 +1,89 @@
-const NoteServices = require('../services/NoteServices');
+const NoteService = require('../services/NoteServices');
 
 
 module.exports = {
     ping: (req, res) => {
         res.json({pong:true});
     },
-    all: () => {
 
+    all: async (req, res) => {
+        let json = {error:'', result:[]};
+
+        let notes = await NoteService.getall();
+
+        for(let i in notes) {
+            json.result.push({
+                id: notes[i].id,
+                title: notes[i].title
+            });
+        }
+
+        res.json(json);
     },
-    one: () => {
+    one: async (req, res) => {
+        let json = {error:'', result:{}};
 
+        let id = req.params.id;
+        let note = await NoteService.findById(id);
+
+        if(note){ 
+            json.result = note;
+        }
+
+        res.json(json);
     },
-    new: () => {
+    new: async (req, res) => {
+        let json = {error:'', result:{}};
 
+        let title = req.body.title;
+        let body = req.body.body;
+
+        if(title && body){
+
+            let noteId = await NoteService.add(title,body);
+            json.result = {
+                id: noteId,
+                title,
+                body
+            };
+
+
+        }else{
+            json.error = 'Campos nao enviados';
+        }
+
+
+        res.json(json);
     },
-    edit: () => {
+    edit: async (req, res) => {
+        let json = {error:'', result:{}};
 
+        let id = req.params.id;
+        let title = req.body.title;
+        let body = req.body.body;
+
+        if(id && title && body){
+
+            await NoteService.update(id, title, body);
+            json.result = {
+                id,
+                title,
+                body
+            };
+
+
+        }else{
+            json.error = 'Campos nao enviados';
+        }
+
+
+        res.json(json);
     },
-    delete: () => {
+    delete: async ( req, res) => {
+        let json = {error:'', result:{}};
 
+        await NoteService.delete(req.params.id);
+
+        res.json(json);
     },
 };
