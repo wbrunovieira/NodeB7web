3fa5eb6c5c6ba8e702aacc42ed37cec94c49a0fd
wbrunovieira <wbrunovieira77@gmail.com>
2021-02-04 17:12:02 +0000

    made the page layout and the socket connection

--- chatNode/server.js
+++ chatNode/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketIo(server);
 
-server.listen(3030);
+
+server.listen(3030, console.log('servidor rodando'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('conexao conectada');
+    
+});
