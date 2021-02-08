d7f452fe6bfde38f8ae2f03baf776b3bd4f8a26a
wbrunovieira <wbrunovieira77@gmail.com>
2021-02-08 15:59:29 +0000

    finished the chat project, learned how to work with socket

--- chatNode/server.js
+++ chatNode/server.js
@@ -12,7 +12,42 @@ server.listen(3030, console.log('servidor rodando'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+let connectedUsers = [];
+
 io.on('connection', (socket) => {
     console.log('conexao conectada');
-    
+
+    socket.on('join-request', (username) => {
+        socket.username = username;
+        connectedUsers.push( username);
+        console.log(connectedUsers);
+
+        socket.emit('user-ok', connectedUsers);
+        socket.broadcast.emit('list-update', { 
+            joined: username,
+            list: connectedUsers
+        });
+    });
+
+    socket.on('disconnect', () => {
+        connectedUsers = connectedUsers.filter( u => u != socket.username);
+        console.log(connectedUsers);
+
+        socket.broadcast.emit('list-update', { 
+           left: socket.username,
+           list: connectedUsers 
+        });
+
+    });
+
+    socket.on('send-msg', (txt) => {
+        let obj = {
+            username: socket.username,
+            message: txt
+        };
+
+        // socket.emit('show-msg', obj);
+        socket.broadcast.emit('show-msg', obj);
+    });
+
 });
